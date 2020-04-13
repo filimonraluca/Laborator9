1c9939c7accb1918cf9f306c89b24b578780feeb
Raluca Filimon <filimon.raluca@gmail.com>
2020-04-13 13:16:26 +0300

    added video

--- src/main/java/app/AlbumManager.java
+++ src/main/java/app/AlbumManager.java
@@ -15,15 +15,21 @@ public class AlbumManager {
         Artist artist1 = new Artist("Raluca", "Italia");
         Artist artist2 = new Artist("Robert", "Rusia");
         Artist artist3 = new Artist("Maria", "Spania");
+        Artist artist4 = new Artist("Maria", "Italia");
+        Artist artist5 = new Artist("George", "Spania");
         ArtistRepository artistRepository = new ArtistRepository(persistenceUtil.getEntityManagerFactory());
         artistRepository.create(artist1);
         artistRepository.create(artist2);
         artistRepository.create(artist3);
+        artistRepository.create(artist4);
+        artistRepository.create(artist5);
 
+        System.out.println("------Artist by id ---------");
         Artist findedArtistByID = artistRepository.findById(3);
         System.out.printf("Artist found by id %s\n",findedArtistByID.toString());
 
-        List<Artist> findedArtistByName = artistRepository.findByName("Raluca");
+        System.out.println("------Artist by name ---------");
+        List<Artist> findedArtistByName = artistRepository.findByName("Maria");
         for (Artist a : findedArtistByName) {
             System.out.printf("Artist found by name %s\n", a.toString());
         }
@@ -32,22 +38,28 @@ public class AlbumManager {
         Album album2 = new Album("titlu2",1, 1990);
         Album album3 = new Album("titlu3",2, 2001);
         Album album4 = new Album("titlu1",3, 2010);
+        Album album5 = new Album("titlu1",2, 2012);
         AlbumRepository albumRepository = new AlbumRepository(persistenceUtil.getEntityManagerFactory());
         albumRepository.create(album1);
         albumRepository.create(album2);
         albumRepository.create(album3);
         albumRepository.create(album4);
-        Album albumFoundById = albumRepository.findById(3);
-        System.out.printf("Album found by id %s", albumFoundById.toString());
+        albumRepository.create(album5);
 
+        System.out.println("------Album by id ---------");
+        Album albumFoundById = albumRepository.findById(3);
+        System.out.printf("Album found by id %s\n", albumFoundById.toString());
+
+        System.out.println("------Album by name ---------");
         List<Album> foundbyName = albumRepository.findByName("titlu1");
         for (Album a : foundbyName){
-            System.out.printf("\nAlbum found by name %s\n", a.toString());
+            System.out.printf("Album found by name %s\n", a.toString());
         }
 
+        System.out.println("------Album by artist ---------");
         List<Album> foundbyArtist = albumRepository.findByArtist(artist1);
         for (Album a : foundbyArtist){
-            System.out.printf("\nAlbum found by artist %s\n", a.toString());
+            System.out.printf("Album found by artist %s\n", a.toString());
         }
     }
 }
